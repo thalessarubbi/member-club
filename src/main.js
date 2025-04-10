0eacceda22f420c741c05f8a35653ed80871a09f
Thales Sarubbi <thalessarubbi@gmail.com>
2025-04-09 23:08:45 -0300

    build: :building_construction: setup webpack and babel

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
+"use strict";
+
+//CSS
+import './styles/global.css'
+import './styles/header.css'
+import './styles/user-card.css'
+import './styles/fidelity-card.css'
+import './styles/cuts-card.css'
+import './styles/history-card.css'
 
 import './modules/form/submit.js'
